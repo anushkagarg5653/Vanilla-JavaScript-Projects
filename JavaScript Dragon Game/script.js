3238a1ee4799a422a3005c61fc53501430edcf71
anushkagarg5653 <anushkagarg0811@gmail.com>
2020-12-02 18:50:01 +0530

    initial commit

---
+++ JavaScript Dragon Game/script.js
@@ -0,0 +1,10 @@
+document.onkeydown = function(e){
+    console.log("key code is:", e.keyCode)
+    if(e.keyCode==38){
+        dino= document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(()=> {
+            dino.classList.remove('animateDino')
+        }, 700);
+    }
+}
